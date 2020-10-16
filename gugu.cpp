6f9bae26a72b1404fe91ebfcaf37c700a3257c31
Hakjoo Lee <hakjoo@khu.ac.kr>
2020-10-16 11:05:27 +0900

    Add 8 table

---
+++ gugu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	for (int i = 2; i <= 8; i++)
+	{
+
+		for (int j = 1; j <= 9; j++)
+		{
+			cout << i << " * " << j << " = " << i * j << "   ";
+		}
+		cout << "\n";
+	}
+
+
+
+
+	return 0;
+}
